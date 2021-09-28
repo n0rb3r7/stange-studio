485af0d1ff6d38ca0a43e4bde9ca5d60f289c476
Norbert <n0rb3r7@outlook.com>
2021-09-27 23:15:11 -0300

    Minimal fixes on TheNavBar and TheFooter.

--- layouts/TheNavBar.vue
+++ layouts/TheNavBar.vue
@@ -1,27 +1,25 @@
 <template>
-  <div>
-    <b-navbar>
-      <template #brand>
-        <b-navbar-item tag="router-link" :to="{ path: '/' }">
-          <StangeStudioLogo :height="40" :width="40" />
-        </b-navbar-item>
-      </template>
-      <template #end>
-        <b-navbar-item v-for="(item, key) of items" :key="key">
-          <NuxtLink :to="item.to" exact-active-class="is-active">
-            <b-icon :icon="item.icon" /> {{ item.title }}
+  <b-navbar>
+    <template #brand>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <StangeStudioLogo :height="35" :width="35" />
+      </b-navbar-item>
+    </template>
+    <template #end>
+      <b-navbar-item v-for="(item, key) of items" :key="key">
+        <NuxtLink :to="item.to" exact-active-class="is-active">
+          <span>{{ item.title }}</span>
+        </NuxtLink>
+      </b-navbar-item>
+      <b-navbar-item>
+        <div class="buttons">
+          <NuxtLink class="button is-danger" to="/contact">
+            <strong>Contacto</strong>
           </NuxtLink>
-        </b-navbar-item>
-        <b-navbar-item tag="div">
-          <div class="buttons">
-            <NuxtLink class="button is-danger" to="/contact">
-              <strong>Contacto</strong>
-            </NuxtLink>
-          </div>
-        </b-navbar-item>
-      </template>
-    </b-navbar>
-  </div>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
 </template>
 
 <script>
